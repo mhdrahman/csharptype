0fe7808e4b319575153e79997970cfad51658d3e
mhdrahman <mhd.rahman601@gmail.com>
2023-12-18 19:47:25 +0000

    Add project files.

---
+++ CSharpType/GameData.cs
@@ -0,0 +1,213 @@
+﻿using System.Text;
+using CrypticWizard.RandomWordGenerator;
+
+namespace CSharpType
+{
+    internal static class GameData
+    {
+        public static List<string> Snippets = new()
+        {
+            "var list = new List<string>();",
+            "var list = new List<string>(){};",
+            "var list = new List<string>{};",
+            "List<string> list = new();",
+            "List<string> list = new(){};",
+            "List<string> list;",
+
+            "var dictionary = new Dictionary<string, string>();",
+            "var dictionary = new Dictionary<string, string>(){};",
+            "var dictionary = new Dictionary<string, string>{};",
+            "Dictionary<string, string> dictionary = new();",
+            "Dictionary<string, string> dictionary = new(){};",
+            "Dictionary<string, string> dictionary;",
+
+            "var hashSet = new HashSet<string>();",
+            "var hashSet = new HashSet<string>(){};",
+            "var hashSet = new HashSet<string>{};",
+            "HashSet<string> hashSet = new();",
+            "HashSet<string> hashSet = new(){};",
+            "HashSet<string> hashSet;",
+
+            "public bool Property { get; set; }",
+            "public bool Property { get; internal set; }",
+            "public bool Property { get; private set; }",
+            "public bool Property { get; }",
+
+            "internal bool Property { get; set; }",
+            "internal bool Property { get; internal set; }",
+            "internal bool Property { get; private set; }",
+            "internal bool Property { get; }",
+
+            "private readonly string _foo;",
+            "private readonly string _foo = \"bar\";",
+
+            "public static void Method(){}",
+            "public static string Method(){}",
+            "public static int Method(){}",
+            "public static bool Method(){}",
+            "public static Task Method(){}",
+
+            "public void Method(){}",
+            "public string Method(){}",
+            "public int Method(){}",
+            "public bool Method(){}",
+            "public Task Method(){}",
+
+            "public async Task Method(){}",
+            "public async Task<string> Method(){}",
+            "public async Task<int> Method(){}",
+            "public async Task<bool> Method(){}",
+
+            "(a, b) => true",
+            "(a, b) => { return true; }",
+            "(a, _) => { return true; }",
+            "(_, _) => { return true; }",
+
+            "foreach (var item in collection){}",
+            "for (var i = 0; i < collection.Count; i++){}",
+            "while(true){}",
+
+            "if (true){}",
+            "if (true){} else{}",
+            "if (true){} else if(true){} else{}",
+        };
+
+        public static List<string> Keywords = new()
+        {
+            "abstract",
+            "as",
+            "base",
+            "bool",
+            "break",
+            "byte",
+            "case",
+            "catch",
+            "char",
+            "checked",
+            "class",
+            "const",
+            "continue",
+            "decimal",
+            "default",
+            "delegate",
+            "do",
+            "double",
+            "else",
+            "enum",
+            "event",
+            "explicit",
+            "extern",
+            "false",
+            "finally",
+            "fixed",
+            "float",
+            "for",
+            "foreach",
+            "goto",
+            "if",
+            "implicit",
+            "in",
+            "int",
+            "interface",
+            "internal",
+            "is",
+            "lock",
+            "long",
+            "namespace",
+            "new",
+            "null",
+            "object",
+            "operator",
+            "out",
+            "override",
+            "params",
+            "private",
+            "protected",
+            "public",
+            "readonly",
+            "ref",
+            "return",
+            "sbyte",
+            "sealed",
+            "short",
+            "sizeof",
+            "stackalloc",
+            "static",
+            "string",
+            "struct",
+            "switch",
+            "this",
+            "throw",
+            "true",
+            "try",
+            "typeof",
+            "uint",
+            "ulong",
+            "unchecked",
+            "unsafe",
+            "ushort",
+            "using",
+            "virtual",
+            "void",
+            "volatile",
+            "while",
+            "add",
+            "and",
+            "alias",
+            "ascending",
+            "args",
+            "async",
+            "await",
+            "by",
+            "descending",
+            "dynamic",
+            "equals",
+            "file",
+            "from",
+            "get",
+            "global",
+            "group",
+            "init",
+            "into",
+            "join",
+            "let",
+            "managed",
+            "nameof",
+            "nint",
+            "not",
+            "notnull",
+            "nuint",
+            "on",
+            "or",
+            "orderby",
+            "partial",
+            "record",
+            "remove",
+            "required",
+            "scoped",
+            "select",
+            "set",
+            "unmanaged",
+            "value",
+            "var",
+            "when",
+            "where",
+            "with",
+            "yield",
+        };
+
+        public static string GenerateSentence()
+        {
+            var rng = new Random();
+            var phrase = new StringBuilder();
+            var wordGenerator = new WordGenerator();
+            for (int i = 0; i < 5; i++)
+            {
+                // phrase.Append($"{Keywords[rng.Next(0, Keywords.Count)]} ");
+                phrase.Append($"{wordGenerator.GetWord()} ");
+            }
+
+            return phrase.ToString().Substring(0, phrase.Length - 1);
+        }
+    }
+}
---
+++ CSharpType/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+
+namespace CSharpType
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var choices = GameData.Snippets;
+            var rng = new Random();
+            while (true)
+            {
+                Console.Clear();
+                var target = choices[rng.Next(0, choices.Count)];
+                //target = GameData.GenerateSentence();
+                var inputPhrase = "";
+                var index = 0;
+                Console.Write(target);
+                Console.CursorLeft = index;
+                while (inputPhrase != target)
+                {
+                    var input = Console.ReadKey();
+                    if (input.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+
+                    if (input.Key == ConsoleKey.Backspace)
+                    {
+                        if (index != 0)
+                        {
+                            index--;
+                            inputPhrase = inputPhrase.Remove(inputPhrase.Length - 1);
+                        }
+
+                        Console.CursorLeft = index;
+                        if (index > target.Length - 1)
+                        {
+                            Console.Write(" ");
+                        }
+                        else
+                        {
+                            Console.Write(target[index]);
+                        }
+
+                        Console.CursorLeft = index;
+                        continue;
+                    }
+
+                    Console.CursorLeft = index;
+                    inputPhrase += input.KeyChar;
+                    if (index > target.Length - 1)
+                    {
+                        WriteCharWithColor(input.KeyChar, ConsoleColor.Red);
+                        index++;
+                    }
+                    else if (input.KeyChar != target[index])
+                    {
+                        WriteCharWithColor(target[index], ConsoleColor.Red);
+                        index++;
+                    }
+                    else
+                    {
+                        WriteCharWithColor(input.KeyChar, ConsoleColor.Green);
+                        index++;
+                    }
+                }
+            }
+        }
+
+        private static void WriteCharWithColor(char character, ConsoleColor color)
+        {
+            var originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(character);
+            Console.ForegroundColor = originalColor;
+        }
+    }
+}
